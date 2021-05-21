818d887ea752ae9c62f0630c7fda8eb827ec7142
Sherick16 <ajaxsdvbc3to@Gmail.com>
2021-05-21 21:52:35 +0200

    initial commit

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    [SerializeField] private float minY = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < minY)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
